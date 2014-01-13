e91fd389bf7207a3eb04f8b5f4d5bfa87a056ded
Zoaldyeck <tacoskebab@hotmail.fr>
2014-01-13 15:13:25 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -3,15 +3,6 @@
  * You can add or remove any command here.
  */
 
-/********
-module.exports {
-hour: function hour() {
-	if(mess.message == "/hour")
-		socket.emit('command', current_hour);
-	});
-}});
-}
-**********/
 module.exports;
 
 module.exports = function() {
