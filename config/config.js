d255698e7d62c63e8f9a9dafe3a301d3e45ea26d
Zoaldyeck <tacoskebab@hotmail.fr>
2014-01-14 00:24:09 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 /*
- * Do not forget to edit the port if necessary.
- * Default is 8000 (server-side AND client-side).
- * Then, do not forget to edit the file /static/js/functions.js
- * on the firsts lines to set the hosts informations (IP, domain name
- * and port).
+ * You can edit the port if necessary.
+ * (default is 8000)
+ * Then, please do not forget to edit the file /static/js/functions.js
+ * in the nodeChat-Client on the firsts lines to set the hosts 
+ * informations (IP, domain name and port).
  */
 PORT = 8000;
